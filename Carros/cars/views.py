1120b3ee691f4c143cb36df8ca2dda94d0fbfe4f
armenioandrade <armenioandrade@gmail.com>
2024-08-21 22:17:02 -0300

    avançando com views, templates e djando template language

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from cars.models import Car
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+def cars_view(request):
+    cars = Car.objects.all()
+    print('chamdou o cars_view')
+    print(cars)
+    return render(request, 
+                  'cars.html', 
+                  {'cars': cars})
