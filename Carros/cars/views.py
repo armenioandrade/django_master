1df1c7deb88e57cd61110aeed6370d8dd73e2d1e
Armenio <armenioandrade@gmail.com>
2024-09-24 23:17:59 -0300

    finalizado decorators para login

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -1,5 +1,7 @@
 from typing import Any
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from cars.models import Car
 from cars.forms import CarModelForm
 
@@ -15,24 +17,26 @@ class CarsListView(ListView):
         if search:
             cars = cars.filter(model__icontains=search)
         return cars
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
     
-    
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     template_name = 'new_car.html'
     form_class = CarModelForm
     success_url = '/cars'
 
-class CarDetailView(DetailView):
-    model = Car
-    template_name = 'car_detail.html'
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     success_url = '/cars'
     template_name = 'car_update.html'
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
